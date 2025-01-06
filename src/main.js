a268620ad15554597c31c6947287940d9f8b3512
MaslowskiLukasz <l.maslowski2012@gmail.com>
2025-01-06 23:25:30 +0100

    remove comment

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import './style.css'
 import { initGameState } from './controls'
-import { render, update } from './render';
+import { render } from './render';
 
 initGameState();
 
 window.requestAnimationFrame(gameLoop);
 
 function gameLoop() {
-  // update();
   render();
   window.requestAnimationFrame(gameLoop);
 }
